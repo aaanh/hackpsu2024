cf77ccccea2b8d2cd6d98eb35f66760713fea3f1
Anh H Nguyen <anhnguyen@aaanh.com>
2024-03-16 19:38:50 -0400

    Scaffolding

---
+++ frontend/components/NavBar.tsx
@@ -0,0 +1,45 @@
+"use client";
+
+import * as React from "react";
+import Link from "next/link";
+
+import { cn } from "@/lib/utils";
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  navigationMenuTriggerStyle
+} from "@/components/ui/navigation-menu";
+import { ThemeToggle } from "./theme-toggler";
+import { FaHome, FaUser } from "react-icons/fa";
+
+export function NavBar() {
+  return (
+    <NavigationMenu className="justify-between min-w-full flex p-2">
+      <NavigationMenuList>
+        <NavigationMenuItem>
+          <Link href="/" legacyBehavior passHref>
+            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+              <FaHome></FaHome>
+            </NavigationMenuLink>
+          </Link>
+        </NavigationMenuItem>
+        <NavigationMenuItem>
+          <Link href="/profile" legacyBehavior passHref>
+            <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+              <FaUser></FaUser> &nbsp; Profile
+            </NavigationMenuLink>
+          </Link>
+        </NavigationMenuItem>
+      </NavigationMenuList>
+      <NavigationMenuList className="ml-auto">
+        <NavigationMenuItem>
+          <ThemeToggle></ThemeToggle>
+        </NavigationMenuItem>
+      </NavigationMenuList>
+    </NavigationMenu>
+  );
+}
