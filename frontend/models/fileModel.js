1c5ac52073ac337b93d1d48e3a4d3b379db905c6
seansongss <seansongss@gmail.com>
2024-03-17 10:03:23 -0400

    python access db using fastapi

--- frontend/models/fileModel.js
+++ frontend/models/fileModel.js
@@ -1,11 +1,12 @@
 import { Schema, model, models } from 'mongoose';
 
 const fileSchema = new Schema({
-    // user_id: String,
+    user_id: String,
     public_id: String,
     secure_url: String,
+    session_id: String
 }, { timestamps: true });
 
-const FileModel = models.File || model('File', fileSchema);
+const FileModel = models.File || model('FileModel', fileSchema);
 
 export default FileModel;
