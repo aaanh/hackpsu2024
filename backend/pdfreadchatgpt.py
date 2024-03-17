25c8a3f89de8535e4f355f53ea4eb4ebd8f63016
seansongss <seansongss@gmail.com>
2024-03-17 05:55:49 -0400

    pdfResponse function

--- backend/pdfreadchatgpt.py
+++ backend/pdfreadchatgpt.py
@@ -1,5 +1,3 @@
-# je veux mourir
-
 import os
 from openai import OpenAI
 import requests
@@ -11,7 +9,8 @@ import fitz
 pdf_url = "https://res.cloudinary.com/daseayvyl/image/upload/v1710653761/hackspsu2024/n6q0cbiqafofumssgtta.pdf"
 
 # test pdf of resume
-# pdf_url = "https://res.cloudinary.com/daseayvyl/image/upload/v1710654150/hackspsu2024/wqbp9on9628o77wisqdx.pdf" 
+# pdf_url = "https://res.cloudinary.com/daseayvyl/image/upload/v1710654150/hackspsu2024/wqbp9on9628o77wisqdx.pdf"
+openai_api_key = "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"
 
 
 # Changing a PDF to text
@@ -29,8 +28,6 @@ if response.status_code == 200:
 else:
     print("Failed to download the PDF")
 
-# TODO: dotenv api key in the future
-openai_api_key = "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"
 client = OpenAI(api_key=openai_api_key)
 
 def format_message(role, content):
@@ -38,39 +35,18 @@ def format_message(role, content):
 
 def get_response(messages):
     completion = client.chat.completions.create(
-        model='gpt-3.5-turbo', # use 'gpt-4-turbo' for the longer, specific response, but takes 20 seconds to respond
+        # use 'gpt-4-turbo' for the longer, specific response, but takes 20 seconds to respond
+        model='gpt-3.5-turbo',
         messages=messages,
     )
     content = completion.choices[0].message.content
     return content
 
-default_msg = format_message("system", "You are an online study tool's chatbot. Use point form for summarizing.")
-user_msg = format_message("user", "Summarize the following content:\n\n" + text)
+default_msg = format_message(
+    "system", "You are an online study tool's chatbot. Use point form for summarizing.")
+user_msg = format_message(
+    "user", "Summarize the following content:\n\n" + text)
 
 response = get_response([default_msg, user_msg])
 
 print(response)
-
-
-# take in pdf
-# file = client.files.create(
-#     file=pdf_bytes,
-#     purpose='assistants'
-# )
-
-# # chatgpt with files inputted supposedly
-# helper = client.beta.assistants.create(
-#     instructions="You are an online study tool's chatbot. Use your knowledge base to best respond to the student's queries.",
-#     model="gpt-4-turbo-preview",
-#     tools=[{"type": "retrieval"}],
-#     file_ids=[file.id]
-# )
-
-# thread = client.beta.threads.create()
-
-# thread_message = client.beta.threads.messages.create(
-#     thread_id=thread.id,
-#     role="user",
-#     content="summarize the content in a sentence for me please.",
-#     file_ids=[file.id]
-# )
