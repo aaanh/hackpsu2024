f49eeee0384e49e651c6680ed843d22979c6351f
Anh H Nguyen <anhnguyen@aaanh.com>
2024-03-17 02:14:44 -0400

    Fix tsx errors

--- frontend/app/page.tsx
+++ frontend/app/page.tsx
@@ -1,3 +1,18 @@
+"use client";
+
+import UploadForm from "@/components/UploadForm";
+import { useUser } from "@auth0/nextjs-auth0/client";
+
 export default function Home() {
-  return <main className=""></main>;
+  const user = useUser();
+
+  return (
+    <main className="w-full flex items-center justify-center flex-col">
+      {user.user ? (
+        <UploadForm></UploadForm>
+      ) : (
+        "Welcome! Please log in to upload files."
+      )}
+    </main>
+  );
 }
